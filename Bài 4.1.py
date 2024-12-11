1e7e5d7a39c2a2ca70deef25345d9e6c665264c7
daoquanghuy2005 <daoquanghuyql2005@gmail.com>
2024-12-11 13:49:16 +0700

    Add files via upload

---
+++ Bài 4.1.py
@@ -0,0 +1,7 @@
+print('Đào Quang Huy MSSV 235752021610051')
+# Nhập chuỗi S từ bàn phím
+S = input("Nhập chuỗi S: ")
+
+# In ra từng kí tự của S, mỗi kí tự trên một dòng
+for char in S:
+    print(char)
